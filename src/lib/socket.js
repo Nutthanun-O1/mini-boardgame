d7e820ff87d128c9409fc56ed6588786c15f4ace
Nutthanun-O1 <nutthanunnunu@gmail.com>
2026-03-04 15:16:51 +0700

    sever

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -3,4 +3,11 @@
 import { io } from 'socket.io-client';
 
 const URL = process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:3001';
-export const socket = io(URL);
+
+export const socket = io(URL, {
+  transports: ['websocket', 'polling'],
+  autoConnect: true,
+  reconnection: true,
+  reconnectionAttempts: 10,
+  reconnectionDelay: 1000,
+});
